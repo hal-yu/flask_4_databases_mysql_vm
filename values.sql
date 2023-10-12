580567a8680c805c1da5bc6227ed9174a6e8cd0d
yu-hal <haley.yu@stonybrook.edu>
2023-10-12 03:21:17 +0000

    update chuuya name

--- values.sql
+++ values.sql
@@ -7,7 +7,7 @@ VALUES
 ('Atsumu', 'Miya', 'Psychiatry', '6461115454'),
 ('Osamu', 'Miya', 'Internal Medicine', '6461115456'),
 ('Satoru', 'Gojo', 'Cardiology', '6462221314'),
-('Chuuya', 'Nakhara', 'Pediatrics', '6461211218');
+('Chuuya', 'Nakahara', 'Pediatrics', '6461211218');
 
 INSERT INTO patients(
     first_name,
